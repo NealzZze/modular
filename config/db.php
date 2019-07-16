6f1e0ac3aceb16a5f6940fee0c5454a3cfe4903e
NealzZze <inealser@cpanel21.hostiman.ru>
2019-07-16 11:20:34 +0300

    changes for host

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
     return [
        'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=127.0.0.1;dbname=modular',
-        'username' => 'mod_user',
-        'password' => 'root',
+        'dsn' => 'mysql:host=127.0.0.1;dbname=inealser_modular',
+        'username' => 'inealser_moduser',
+        'password' => '140998Niki',
         'charset' => 'utf8',
      ];
